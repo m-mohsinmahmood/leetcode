c712b3ce807b3815d5767b3db141bb3e41700acb
MMohsinMahmood <2mohsin.mahmood@gmail.com>
2025-10-07 11:25:44 +0500

    Palindrome Number

--- solution.js
+++ solution.js
@@ -1,50 +1,44 @@
 /**
- * Problem: Two Sum
+ * Problem: Palindrome Number
  * Difficulty: Easy
 
-* Question: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-  You may assume that each input would have exactly one solution, and you may not use the same element twice.
-  You can return the answer in any order.
+* Question: Given an integer x, return true if x is a palindrome, and false otherwise.
 
   Example 1:
-  Input: nums = [2,7,11,15], target = 9
-  Output: [0,1]
-  Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+  Input: x = 121
+  Output: true
+  Explanation: 121 reads as 121 from left to right and from right to left.
 
   Constraints:
-  2 <= nums.length <= 104
-  -109 <= nums[i] <= 109
-  -109 <= target <= 109
+  -231 <= x <= 231 - 1
  */
 
 console.clear();
 const now = new Date();
 console.log(`\n===== Running Solution at ${now.toLocaleString()} =====`);
 
-const problemTitle = "Two Sum"; // e.g., "Two Sum", "Reverse Linked List", etc.
+const problemTitle = "Palindrome Number";
 console.log(`🧩 Problem: ${problemTitle}\n`);
 
-function func(nums, target) {
-  result = [];
+function func(x) {
+  result = false;
 
-  if (nums.length >= 2 && nums.length <= Math.pow(10, 4)) {
-    for (i = 0; i < nums.length; i++) {
-      if ((nums[i] >= Math.pow(-10, 9) && nums[i] <= Math.pow(10, 9)) && (target >= Math.pow(-10, 9) && target <= Math.pow(10, 9))) {
-        for (j = 1; j < nums.length; j++) {
-          if (nums[i] + nums[j] == target && i != j) {
-            if (!result.includes(i))
-              result.push(i);
-            if (!result.includes(j))
-              result.push(j);
-          }
+  if (x >= Math.pow(-2, 31) && x <= (Math.pow(2, 31) - 1)) {
+    number = x.toString()
 
-        }
-
+    for (i = 0; i < number.length; i++) {
+      if (number[i] == number[((number.length) - 1) - i]) {
+        result = true;
+      }
+      else {
+        result = false;
+        break;
       }
     }
   }
+
   return result;
 }
 
 // Example run:
-console.log(func([2, 7, 11, 15], 9)); // Output: [0, 1]
+console.log(func(1001)); // Output: [0, 1]
